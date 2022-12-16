b391fa4f9d6abc15645d4afc3a99a980c6aeb5a9
Fe Macaraeg <femacaraeg@gmail.com>
2022-12-16 16:50:09 +0800

    Handle logged in route.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,28 @@
-import '../styles/globals.css';
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
+import { magic } from '../lib/magic-client';
 import type { AppProps } from 'next/app';
 import { Roboto_Slab } from '@next/font/google';
+import '../styles/globals.css';
 
 const roboto = Roboto_Slab({
   subsets: ['latin'],
 });
 export default function App({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleLoggedIn = async () => {
+      const isLoggedIn = await magic?.user.isLoggedIn();
+      if (isLoggedIn) {
+        router.push('/');
+      } else {
+        router.push('/login');
+      }
+    };
+    handleLoggedIn();
+  }, []);
+
   return (
     <main className={roboto.className}>
       <Component {...pageProps} />
