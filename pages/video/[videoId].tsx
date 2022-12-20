f53101ce810bfdc9322152a8938eee125faea197
Fe Macaraeg <femacaraeg@gmail.com>
2022-12-20 20:38:27 +0800

    Add iframe youtube player.

--- pages/video/[videoId].tsx
+++ pages/video/[videoId].tsx
@@ -21,6 +21,12 @@ function Video() {
         overlayClassName={styles.overlay}
         className={styles.modal}
       >
+        <iframe
+          id='ytplayer'
+          width='100%'
+          height='360'
+          src={`https://www.youtube.com/embed/${router.query.videoId}?autoplay=0&origin=http://example.com&controls=0&rel=1`}
+        ></iframe>
         <div>Modal body</div>
       </Modal>
     </div>
