9b92a0839ff3e46ac2a9e0e2933b14a52d5e074d
Fe Macaraeg <femacaraeg@gmail.com>
2022-12-20 18:54:13 +0800

    Add Video page as modal.

--- pages/video/[videoId].tsx
+++ pages/video/[videoId].tsx
@@ -1,12 +1,30 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
+import Modal from 'react-modal';
+
+import styles from '../../styles/Video.module.css';
 
 import { useRouter } from 'next/router';
+
+Modal.setAppElement('#__next');
 
 function Video() {
   const router = useRouter();
 
   console.log({ router });
-  return <div>Video Page {router.query.videoId}</div>;
+  return (
+    <div className={styles.container}>
+      <Modal
+        isOpen={true}
+        contentLabel='Watch the video'
+        onRequestClose={() => router.back()}
+        overlayClassName={styles.overlay}
+        className={styles.modal}
+      >
+        <div>Modal body</div>
+      </Modal>
+    </div>
+  );
 }
 
 export default Video;
